ae049293952bc2a08906a06a574cf87df60a75d0
Lynette Okoroike <lcokoroike@gmail.com>
2018-09-17 21:25:25 -0700

    Turned it into my official poetry website.

---
+++ lcopoetry.js
@@ -0,0 +1,38 @@
+var config = {
+    apiKey: "AIzaSyAYLsO_pwspkIOQo6sEBAOs16IrkfrTywE",
+    authDomain: "lco-poetry-contact-page.firebaseapp.com",
+    databaseURL: "https://lco-poetry-contact-page.firebaseio.com",
+    projectId: "lco-poetry-contact-page",
+    storageBucket: "lco-poetry-contact-page.appspot.com",
+    messagingSenderId: "1020825407703"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+  $("#button").on("click", function (e) {
+    e.preventDefault()
+    console.log($(this).text());
+    var name=$("#name").val().trim()
+    console.log(name);
+    var email=$("#email").val().trim()
+    console.log(email);
+    var message=$("#message").val().trim()
+    console.log(message);
+    database.ref().push({
+        name:name,
+        email:email,
+        message:message,
+    })
+  });
+  database.ref().on("child_added", function (snapshot) {
+    console.log(snapshot);
+    var Name = snapshot.val().name;
+    console.log(Name);
+    var Email = snapshot.val().email;
+    console.log(Email);
+    var Message = snapshot.val().message;
+    console.log(Message);
+  },
+  function(err) {
+    console.log(err);
+});
